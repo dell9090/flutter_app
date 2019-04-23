8318061574fc76e1088240ba64c3c5bcfe0414df
dell9090 <liux3721@gmail.com>
2019-04-22 18:56:55 -0700

    initial comment

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       return new MaterialApp(
         title: "Startup Name Generator",
         theme: new ThemeData(
-          primaryColor: Colors.teal,
+          primaryColor: Colors.blue,
         ),
         home: new RandomWords(),
       );
@@ -26,7 +26,7 @@ class RandomWordsState extends State<RandomWords> {
     final _biggerFont = const TextStyle(fontSize: 18.0);
     final _saved = new Set<WordPair>();
 
-    Widget _buildSuggestions() {
+    Widget _buildSuggestions() { // build list view
         return new ListView.builder(
             padding: const EdgeInsets.all(16.0),
             itemBuilder: (context, i) {
@@ -37,7 +37,7 @@ class RandomWordsState extends State<RandomWords> {
                 final index = i ~/ 2;
                 if (index >= _suggestions.length) {
                     //_suggestions.clear();
-                    _suggestions.addAll(generateWordPairs().take(8));
+                    _suggestions.addAll(generateWordPairs().take(10));
 
                 }
                 return _buildRow(_suggestions[index]);
@@ -45,7 +45,7 @@ class RandomWordsState extends State<RandomWords> {
         );
     }
 
-    Widget _buildRow(WordPair pair) {
+    Widget _buildRow(WordPair pair) {// one row of the list
       final alreadySaved = _saved.contains(pair);
 
       return new ListTile(
@@ -53,10 +53,12 @@ class RandomWordsState extends State<RandomWords> {
               pair.asPascalCase,
               style: _biggerFont,
           ),
+
           trailing: new Icon(
             alreadySaved ? Icons.favorite : Icons.favorite_border,
             color: alreadySaved ? Colors.red : null,
           ),
+
           onTap: (){
             setState(() {
               if (alreadySaved) {
